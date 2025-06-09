a9ef0d2e12d3e65f6d8f8ca88f2a81b451884486
DilanM23 <145023346+DilanM23@users.noreply.github.com>
2025-06-08 20:47:43 -0400

    feat: crear modelo personalizado de usuario y formulario de registro

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'turnos', 
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usuarios.Usuario'
+
