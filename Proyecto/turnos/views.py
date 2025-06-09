0e354c9be09adbe4fe634f6852c9bb71845bd066
Maya <mayaoquendo24@gmail.com>
2025-06-08 21:17:01 -0400

    Ponemos el views correspondiente para que el form se vea

--- Proyecto/turnos/views.py
+++ Proyecto/turnos/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DisponibilidadForm
+from .models import Disponibilidad
 
-# Create your views here.
+def crear_disponibilidad(request):
+    if request.method == 'POST':
+        form = DisponibilidadForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ver_disponibilidad')
+    else:
+        form = DisponibilidadForm()
+    return render(request, 'turnos/crear_disponibilidad.html', {'form': form})
+
+def ver_disponibilidad(request):
+    disponibilidades = Disponibilidad.objects.all().order_by('id_doctor', 'dia_semana', 'hora_inicio')
+    return render(request, 'turnos/ver_disponibilidad.html', {'disponibilidades': disponibilidades})
