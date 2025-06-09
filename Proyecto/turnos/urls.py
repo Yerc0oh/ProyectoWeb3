f1a1dfd32e7d781d6a915f8cdcf3b63ffc9574c4
Maya <mayaoquendo24@gmail.com>
2025-06-08 21:30:46 -0400

    Ponemos el url para ver los horarios disponibles

--- Proyecto/turnos/urls.py
+++ Proyecto/turnos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('crear_disponibilidad/', views.crear_disponibilidad, name='crear_disponibilidad'),
     path('ver_disponibilidad/', views.ver_disponibilidad, name='ver_disponibilidad'),
+    path('horarios_disponibles/<int:id_doctor>/<str:dia>/', views.horarios_disponibles, name='horarios_disponibles'),
 ]
