d3b4e24c9a19978290a4359f42a9802ae0fe441b
Maya <mayaoquendo24@gmail.com>
2025-06-08 21:22:44 -0400

    Ponemos otro view que muestre los horarios ya ocupados

--- Proyecto/turnos/views.py
+++ Proyecto/turnos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import DisponibilidadForm
-from .models import Disponibilidad
+from datetime import datetime, timedelta
+from .models import Doctor, Disponibilidad, Turno
 
 def crear_disponibilidad(request):
     if request.method == 'POST':
@@ -15,3 +16,26 @@ def crear_disponibilidad(request):
 def ver_disponibilidad(request):
     disponibilidades = Disponibilidad.objects.all().order_by('id_doctor', 'dia_semana', 'hora_inicio')
     return render(request, 'turnos/ver_disponibilidad.html', {'disponibilidades': disponibilidades})
+
+def horarios_disponibles(request, id_doctor, dia):
+    doctor = Doctor.objects.get(id_doctor=id_doctor)
+    disponibilidad = Disponibilidad.objects.filter(id_doctor=doctor, dia_semana=dia)
+
+    turnos_existentes = Turno.objects.filter(id_doctor=doctor)
+
+    horas_disponibles = []
+
+    for disp in disponibilidad:
+        hora = datetime.combine(datetime.today(), disp.hora_inicio)
+        fin = datetime.combine(datetime.today(), disp.hora_fin)
+        while hora < fin:
+            ocupado = turnos_existentes.filter(fecha_hora__time=hora.time(), fecha_hora__week_day=hora.weekday()+1).exists()
+            if not ocupado:
+                horas_disponibles.append(hora.time())
+            hora += timedelta(minutes=30)
+
+    return render(request, 'turnos/horarios_disponibles.html', {
+        'doctor': doctor,
+        'dia': dia,
+        'horas_disponibles': horas_disponibles
+    })
